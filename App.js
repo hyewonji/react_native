e714f31c66d0588564e9417c2bc823ab90dc979c
hyewonji <60416187+hyewonji@users.noreply.github.com>
2021-06-02 17:07:23 +0900

    test

---
+++ App.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import 'react-native-gesture-handler';
+import React, {Component} from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>match JS and react-native version</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
